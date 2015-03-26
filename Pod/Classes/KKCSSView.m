b52d74b82fb93992a2e9de0f4655a6f26bb880d7
Peter Mackay <peter.mackay@kotikan.com>
2015-03-26 12:36:37 +0000

    Added a layoutSubviews method as a test.

--- Pod/Classes/KKCSSView.m
+++ Pod/Classes/KKCSSView.m
@@ -12,4 +12,24 @@
 
 #pragma mark UIView
 
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    NSArray *subviews = self.subviews;
+    if (subviews.count == 0) {
+        return;
+    }
+    
+    CGFloat width = self.bounds.size.width;
+    CGFloat height = self.bounds.size.height;
+    CGFloat scale = height / subviews.count;
+    
+    for (NSUInteger i = 0; i < subviews.count; ++i) {
+        CGFloat top = i * scale;
+        CGFloat bottom = (i + 1) * scale;
+        UIView *subview = subviews[i];
+        subview.frame = CGRectMake(0, top, width, bottom - top);
+    }
+}
+
 @end
