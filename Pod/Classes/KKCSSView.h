dadd6ab6a9f916339295905c9f3373e091300fbd
Peter Mackay <peter.mackay@kotikan.com>
2015-03-26 11:08:51 +0000

    Specified a custom view in the CSS example.

---
+++ Pod/Classes/KKCSSView.h
@@ -0,0 +1,13 @@
+//
+//  KKCSSView.h
+//  KKCSSLayout
+//
+//  Created by Peter Mackay on 26/03/2015.
+//  Copyright (c) 2014 Peter Mackay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KKCSSView : UIView
+
+@end
---
+++ Pod/Classes/KKCSSView.m
@@ -0,0 +1,15 @@
+//
+//  KKCSSView.m
+//  KKCSSLayout
+//
+//  Created by Peter Mackay on 26/03/2015.
+//  Copyright (c) 2014 Peter Mackay. All rights reserved.
+//
+
+#import "KKCSSView.h"
+
+@implementation KKCSSView
+
+#pragma mark UIView
+
+@end
